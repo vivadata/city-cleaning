5f8535b99a165067cdca537e42209501850be730
Nicolas Thevenin <moloch.kun@gmail.com>
2025-09-25 09:25:55 +0200

    Reactive all the steps

--- dags/test_dbt.py
+++ dags/test_dbt.py
@@ -31,50 +31,54 @@ with DAG(
         field_delimiter=";"
     )
     
-    # transform_to_bq_clvr = GCSToBigQueryOperator(
-    #     task_id="transform_to_bq_clvr",
-    #     destination_project_dataset_table=f"{DESTINATION_PROJECT_DATASET}.colonne_verre",
-    #     bucket=BUCKET_NAME,
-    #     source_objects="dechets-menagers-points-dapport-volontaire-colonnes-a-verre.csv",
-    #     write_disposition='WRITE_TRUNCATE'
-    # )
+    transform_to_bq_clvr = GCSToBigQueryOperator(
+        task_id="transform_to_bq_clvr",
+        destination_project_dataset_table=f"{DESTINATION_PROJECT_DATASET}.colonne_verre",
+        bucket=BUCKET_NAME,
+        source_objects="dechets-menagers-points-dapport-volontaire-colonnes-a-verre.csv",
+        write_disposition='WRITE_TRUNCATE',
+        field_delimiter=";"
+    )
     
-    # transform_to_bq_cpst = GCSToBigQueryOperator(
-    #     task_id="transform_to_bq_cpst",
-    #     destination_project_dataset_table=f"{DESTINATION_PROJECT_DATASET}.composteur",
-    #     bucket=BUCKET_NAME,
-    #     source_objects="dechets-menagers-points-dapport-volontaire-composteurs.csv",
-    #     write_disposition='WRITE_TRUNCATE'
-    # )
+    transform_to_bq_cpst = GCSToBigQueryOperator(
+        task_id="transform_to_bq_cpst",
+        destination_project_dataset_table=f"{DESTINATION_PROJECT_DATASET}.composteur",
+        bucket=BUCKET_NAME,
+        source_objects="dechets-menagers-points-dapport-volontaire-composteurs.csv",
+        write_disposition='WRITE_TRUNCATE',
+        field_delimiter=";"
+    )
     
-    # transform_to_bq_txtl = GCSToBigQueryOperator(
-    #     task_id="transform_to_bq_txtl",
-    #     destination_project_dataset_table=f"{DESTINATION_PROJECT_DATASET}.textile",
-    #     bucket=BUCKET_NAME,
-    #     source_objects="dechets-menagers-points-dapport-volontaire-conteneur-textile.csv",
-    #     write_disposition='WRITE_TRUNCATE'
-    # )
+    transform_to_bq_txtl = GCSToBigQueryOperator(
+        task_id="transform_to_bq_txtl",
+        destination_project_dataset_table=f"{DESTINATION_PROJECT_DATASET}.textile",
+        bucket=BUCKET_NAME,
+        source_objects="dechets-menagers-points-dapport-volontaire-conteneur-textile.csv",
+        write_disposition='WRITE_TRUNCATE',
+        field_delimiter=";"
+    )
     
-    # transform_to_bq_rclr = GCSToBigQueryOperator(
-    #     task_id="transform_to_bq_rclr",
-    #     destination_project_dataset_table=f"{DESTINATION_PROJECT_DATASET}.recyclerie",
-    #     bucket=BUCKET_NAME,
-    #     source_objects="dechets-menagers-points-dapport-volontaire-recycleries-et-ressourceries.csv",
-    #     write_disposition='WRITE_TRUNCATE'
-    # )
+    transform_to_bq_rclr = GCSToBigQueryOperator(
+        task_id="transform_to_bq_rclr",
+        destination_project_dataset_table=f"{DESTINATION_PROJECT_DATASET}.recyclerie",
+        bucket=BUCKET_NAME,
+        source_objects="dechets-menagers-points-dapport-volontaire-recycleries-et-ressourceries.csv",
+        write_disposition='WRITE_TRUNCATE',
+        field_delimiter=";"
+    )
     
-    # transform_to_bq_trlb = GCSToBigQueryOperator(
-    #     task_id="transform_to_bq_trlb",
-    #     destination_project_dataset_table=f"{DESTINATION_PROJECT_DATASET}.trilib",
-    #     bucket=BUCKET_NAME,
-    #     source_objects="dechets-menagers-points-dapport-volontaire-stations-trilib.csv",
-    #     write_disposition='WRITE_TRUNCATE'
-    # )
+    transform_to_bq_trlb = GCSToBigQueryOperator(
+        task_id="transform_to_bq_trlb",
+        destination_project_dataset_table=f"{DESTINATION_PROJECT_DATASET}.trilib",
+        bucket=BUCKET_NAME,
+        source_objects="dechets-menagers-points-dapport-volontaire-stations-trilib.csv",
+        write_disposition='WRITE_TRUNCATE',
+        field_delimiter=";"
+    )
 
     dbt_task = BashOperator(
         task_id="test_dbt",
         bash_command="dbt run",
     )
 
-    # [transform_to_bq_dmr, transform_to_bq_clvr, transform_to_bq_cpst, transform_to_bq_txtl, transform_to_bq_rclr, transform_to_bq_trlb] >> dbt_task
-transform_to_bq_dmr >> dbt_task
+    [transform_to_bq_dmr, transform_to_bq_clvr, transform_to_bq_cpst, transform_to_bq_txtl, transform_to_bq_rclr, transform_to_bq_trlb] >> dbt_task
