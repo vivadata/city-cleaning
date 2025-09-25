ad4f2552670634fbb497c872aa0c30b4cf2cb352
Nicolas Thevenin <moloch.kun@gmail.com>
2025-09-25 16:07:47 +0200

    Fixed staging dmr model

--- dbt-dmr/models/stg/stg_anomalie.sql
+++ dbt-dmr/models/stg/stg_anomalie.sql
@@ -1,9 +1,9 @@
 SELECT
-    `ID DECLARATION` AS id,
-    `TYPE DECLARATION` AS type_declaration,
-    `SOUS TYPE DECLARATION` AS sous_type_declaration ,
-    `CODE POSTAL` AS code_postal,
-    `DATE DECLARATION` AS date_declaration,
+    modele AS id,
+    type AS type_declaration,
+    soustype AS sous_type_declaration ,
+    code_postal AS code_postal,
+    datedecl AS date_declaration,
     INTERVENANT AS intervenant,
     geo_point_2d AS coordonnees
 FROM {{source('city_cleaning', 'dmr')}}
